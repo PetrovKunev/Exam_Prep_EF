aa108b9f9de3a115f844b89f5bd5d37324a5f725
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2024-07-27 12:07:09 +0300

    Model Definition

---
+++ Invoices/Data/DataConstraints.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Invoices.Data
+{
+    public static class DataConstraints
+    {
+        public const byte ProductNameMinLength = 9;
+        public const byte ProductNameMaxLength = 30;
+
+        public const string ProductPriceMinValue = "5.00";
+        public const string ProductPriceMaxValue = "1000.00";
+
+        public const byte AddressStreetNameMinLength = 10;
+        public const byte AddressStreetNameMaxLength = 20;
+
+        public const byte AddressCityMinLength = 5;
+        public const byte AddressCityMaxLength = 15;
+
+        public const byte AddressCountryMinLength = 5;
+        public const byte AddressCountryMaxLength = 15;
+
+        public const int InvoiceNumberMinValue = 1_000_000_000;
+        public const int InvoiceNumberMaxValue = 1_500_000_000;
+
+        public const byte ClientNameMinLength = 10;
+        public const byte ClientNameMaxLength = 25;
+
+        public const byte ClientNumberVatMinLength = 10;
+        public const byte ClientNumberVatMaxLength = 15;
+
+
+    }
+}
